ae44d20a056a2c21f79cdcbbbbf8dc1676b582af
samsonajulor <samsonajulor@gmail.com>
2024-09-03 09:07:35 +0100

    removed db

--- src/app/_lib/queries.ts
+++ src/app/_lib/queries.ts
@@ -1,131 +1,160 @@
-import "server-only"
+import 'server-only'
 
-import { unstable_noStore as noStore } from "next/cache"
-import { db } from "@/db"
-import { tasks, type Task } from "@/db/schema"
-import { type DrizzleWhere } from "@/types"
-import { and, asc, count, desc, gte, lte, or, sql, type SQL } from "drizzle-orm"
+import { unstable_noStore as noStore } from 'next/cache'
+// import { db } from '@/db'
+// import { tasks, type Task } from '@/db/schema'
+// import { type DrizzleWhere } from '@/types'
+// import { and, asc, count, desc, gte, lte, or, sql, type SQL } from 'drizzle-orm'
 
-import { filterColumn } from "@/lib/filter-column"
+// import { filterColumn } from '@/lib/filter-column'
 
-import { type GetTasksSchema } from "./validations"
+import { type GetTasksSchema } from './validations'
 
-export async function getTasks(input: GetTasksSchema) {
+export async function getTasks(input: GetTasksSchema): Promise<{
+    data: {
+        id: string;
+        code: string;
+        title: string | null;
+        status: 'todo' | 'in-progress' | 'done' | 'canceled';
+        label: 'bug' | 'feature' | 'enhancement' | 'documentation';
+        priority: 'low' | 'high';
+        createdAt: Date;
+        updatedAt: Date | null;
+    }[];
+    pageCount: number;
+}> {
   noStore()
-  const { page, per_page, sort, title, status, priority, operator, from, to } =
-    input
+  // const { page, per_page, sort, title, status, priority, operator, from, to } =
+  //   input
 
   try {
-    // Offset to paginate the results
-    const offset = (page - 1) * per_page
-    // Column and order to sort by
-    // Spliting the sort string by "." to get the column and order
-    // Example: "title.desc" => ["title", "desc"]
-    const [column, order] = (sort?.split(".").filter(Boolean) ?? [
-      "createdAt",
-      "desc",
-    ]) as [keyof Task | undefined, "asc" | "desc" | undefined]
+    // // Offset to paginate the results
+    // const offset = (page - 1) * per_page
+    // // Column and order to sort by
+    // // Spliting the sort string by '.' to get the column and order
+    // // Example: 'title.desc' => ['title', 'desc']
+    // const [column, order] = (sort?.split('.').filter(Boolean) ?? [
+    //   'createdAt',
+    //   'desc',
+    // ]) as [keyof Task | undefined, 'asc' | 'desc' | undefined]
 
-    // Convert the date strings to date objects
-    const fromDay = from ? sql`to_date(${from}, 'yyyy-mm-dd')` : undefined
-    const toDay = to ? sql`to_date(${to}, 'yyy-mm-dd')` : undefined
+    // // Convert the date strings to date objects
+    // const fromDay = from ? sql`to_date(${from}, 'yyyy-mm-dd')` : undefined
+    // const toDay = to ? sql`to_date(${to}, 'yyy-mm-dd')` : undefined
 
-    const expressions: (SQL<unknown> | undefined)[] = [
-      title
-        ? filterColumn({
-            column: tasks.title,
-            value: title,
-          })
-        : undefined,
-      // Filter tasks by status
-      !!status
-        ? filterColumn({
-            column: tasks.status,
-            value: status,
-            isSelectable: true,
-          })
-        : undefined,
-      // Filter tasks by priority
-      !!priority
-        ? filterColumn({
-            column: tasks.priority,
-            value: priority,
-            isSelectable: true,
-          })
-        : undefined,
-      // Filter by createdAt
-      fromDay && toDay
-        ? and(gte(tasks.createdAt, fromDay), lte(tasks.createdAt, toDay))
-        : undefined,
-    ]
-    const where: DrizzleWhere<Task> =
-      !operator || operator === "and" ? and(...expressions) : or(...expressions)
+    // const expressions: (SQL<unknown> | undefined)[] = [
+    //   title
+    //     ? filterColumn({
+    //         column: tasks.title,
+    //         value: title,
+    //       })
+    //     : undefined,
+    //   // Filter tasks by status
+    //   !!status
+    //     ? filterColumn({
+    //         column: tasks.status,
+    //         value: status,
+    //         isSelectable: true,
+    //       })
+    //     : undefined,
+    //   // Filter tasks by priority
+    //   !!priority
+    //     ? filterColumn({
+    //         column: tasks.priority,
+    //         value: priority,
+    //         isSelectable: true,
+    //       })
+    //     : undefined,
+    //   // Filter by createdAt
+    //   fromDay && toDay
+    //     ? and(gte(tasks.createdAt, fromDay), lte(tasks.createdAt, toDay))
+    //     : undefined,
+    // ]
+    // const where: DrizzleWhere<Task> =
+    //   !operator || operator === 'and' ? and(...expressions) : or(...expressions)
 
-    // Transaction is used to ensure both queries are executed in a single transaction
-    const { data, total } = await db.transaction(async (tx) => {
-      const data = await tx
-        .select()
-        .from(tasks)
-        .limit(per_page)
-        .offset(offset)
-        .where(where)
-        .orderBy(
-          column && column in tasks
-            ? order === "asc"
-              ? asc(tasks[column])
-              : desc(tasks[column])
-            : desc(tasks.id)
-        )
+    // // Transaction is used to ensure both queries are executed in a single transaction
+    // const { data, total } = await db.transaction(async (tx) => {
+    //   const data = await tx
+    //     .select()
+    //     .from(tasks)
+    //     .limit(per_page)
+    //     .offset(offset)
+    //     .where(where)
+    //     .orderBy(
+    //       column && column in tasks
+    //         ? order === 'asc'
+    //           ? asc(tasks[column])
+    //           : desc(tasks[column])
+    //         : desc(tasks.id)
+    //     )
 
-      const total = await tx
-        .select({
-          count: count(),
-        })
-        .from(tasks)
-        .where(where)
-        .execute()
-        .then((res) => res[0]?.count ?? 0)
+    //   const total = await tx
+    //     .select({
+    //       count: count(),
+    //     })
+    //     .from(tasks)
+    //     .where(where)
+    //     .execute()
+    //     .then((res) => res[0]?.count ?? 0)
 
-      return {
-        data,
-        total,
-      }
-    })
+    //   return {
+    //     data,
+    //     total,
+    //   }
+    // })
 
-    const pageCount = Math.ceil(total / per_page)
-    return { data, pageCount }
+    // const pageCount = Math.ceil(total / per_page)
+    return { data: [{
+        id: '1',
+        code: '1234',
+        title: 'test',
+        status: 'todo',
+        label: 'bug',
+        priority: 'low',
+        createdAt: '12/02/24' as unknown as Date,
+        updatedAt: '12/02/24' as unknown as Date,
+    }], pageCount: 4 }
   } catch (err) {
     return { data: [], pageCount: 0 }
   }
 }
 
-export async function getTaskCountByStatus() {
+export async function getTaskCountByStatus(): Promise<{
+    status: 'todo' | 'in-progress' | 'done' | 'canceled';
+    count: number;
+}[]> {
   noStore()
   try {
-    return await db
-      .select({
-        status: tasks.status,
-        count: count(),
-      })
-      .from(tasks)
-      .groupBy(tasks.status)
-      .execute()
+    // return await db
+    //   .select({
+    //     status: tasks.status,
+    //     count: count(),
+    //   })
+    //   .from(tasks)
+    //   .groupBy(tasks.status)
+    //   .execute()
+    return [{ status: 'todo', count: 2 }]
   } catch (err) {
     return []
   }
 }
 
-export async function getTaskCountByPriority() {
+export async function getTaskCountByPriority(): Promise<{
+    priority: 'low' | 'medium' | 'high';
+    count: number;
+}[]> {
   noStore()
   try {
-    return await db
-      .select({
-        priority: tasks.priority,
-        count: count(),
-      })
-      .from(tasks)
-      .groupBy(tasks.priority)
-      .execute()
+    // return await db
+    //   .select({
+    //     priority: tasks.priority,
+    //     count: count(),
+    //   })
+    //   .from(tasks)
+    //   .groupBy(tasks.priority)
+    //   .execute()
+    return [{ priority: 'low', count: 2 }]
   } catch (err) {
     return []
   }
